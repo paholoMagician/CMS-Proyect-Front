e335046dbc74a865722fe8a5a6c170e58596db3d
Paolo Rivera <v.humandreams@gmail.com>
2025-02-27 15:11:03 -0500

    INIT SAMIEL

--- CMS-System/src/environments/environment.prod.ts
+++ CMS-System/src/environments/environment.prod.ts
@@ -8,7 +8,7 @@ export const environment = {
   
   /**LOCAL DEV */
   // image_url: 'http://localhost:5130/icon-cliente/'
-
+  /** */
   /** DEPLOY */
   image_url: 'http://192.168.55.33:4645/icon-cliente/'
 
